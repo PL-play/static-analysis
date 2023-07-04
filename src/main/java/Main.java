190886dfc81db5f817d884a0f2ddc3ec14ae6723
zhangran653 <zhangran42@126.com>
2023-07-04 12:26:18 +0800

    path checker add to analysis process

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -66,6 +66,9 @@ public class Main {
         parser.addArgument("-es", "--entryselector")
                 .help("entry selectors, choose from 'JspServiceEntry','AnnotationTagEntry','PublicStaticOrMainEntry'. Multiple selectors can be set with ',' in between. Default all");
 
+        parser.addArgument("-pc", "--pathchecker")
+                .help("path checkers. choose from 'default'. Multiple selectors can be set with ',' in between.");
+
         Namespace ns = null;
         try {
             ns = parser.parseArgs(args);
@@ -110,8 +113,13 @@ public class Main {
             analysisExecutor.setTimeout(Integer.parseInt(timeout));
         }
         String es = ns.getString("entryselector");
-        if (timeout != null) {
+        if (es != null) {
             analysisExecutor.setEntrySelector(es);
+        }
+
+        String pc = ns.getString("pathchecker");
+        if (pc != null) {
+            analysisExecutor.setPathChecker(pc);
         }
 
         analysisExecutor.analysis();
